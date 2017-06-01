91f79a4c565f4e17b8eabedf2f435460e8ff35dc
RupaliHangekar <RupaliHangekar@users.noreply.github.com>
2017-06-01 06:44:44 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,12 @@
 def solution(list_of_tuples):
-    '''
-    Enter your code here
-    '''
+    d={}
+    list=[]
+    for k in list_of_tuples:
+        d[k[-1]]=k
+    sd = sorted(d.items())
+    for k,v in sd:
+        list.append(v)
     return list
+
+output=solution([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)])
+print output
