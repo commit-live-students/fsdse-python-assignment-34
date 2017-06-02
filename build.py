1d0730ebdacaae9d4ffcc2940dd59c8336c26bab
mudassirkhan19 <mudassirkhan19@users.noreply.github.com>
2017-06-02 20:24:04 +0000

    Done

--- build.py
+++ build.py
@@ -2,4 +2,11 @@ def solution(list_of_tuples):
     '''
     Enter your code here
     '''
-    return list
+    import operator
+    sort_list = [None] * len(list_of_tuples)
+    for item in list_of_tuples:
+        total = 0
+        for comp_item in list_of_tuples:
+            total += operator.gt(item[-1],comp_item[-1])
+        sort_list[total] = (item)
+    return sort_list
