869a2c4cb845a559ad906be14531011e56adb778
seekingpeace <seekingpeace@users.noreply.github.com>
2017-06-03 08:26:00 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 def solution(list_of_tuples):
-    '''
-    Enter your code here
-    '''
-    return list
+    lis=[]
+    lis=sorted(list_of_tuples,key= lambda x:x[1])
+    return lis
+val = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+print(solution(val))
