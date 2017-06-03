adfb9bfba63d69b95f7a1293a652b74a1b1e44bb
sankbhatt <sankbhatt@users.noreply.github.com>
2017-06-03 08:27:52 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
+import operator
 def solution(list_of_tuples):
-    '''
-    Enter your code here
-    '''
+
+    list = sorted(list_of_tuples,key=operator.itemgetter(1))
+    #list = sorted(list_of_tuples,key=lambda x: x[1])
     return list
+
+d =  [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+print solution(d)
