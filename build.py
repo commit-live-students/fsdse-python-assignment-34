8f33b436e6e7c201822e63271da686a74b9fb660
nixen420 <nikhil.singh4008@gmail.com>
2017-06-22 11:45:56 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
+from operator import itemgetter
+list1=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 def solution(list_of_tuples):
-    '''
-    Enter your code here
-    '''
-    return list
+    list1.sort(key=itemgetter(1))
+    return list1
+solution(list1)    
